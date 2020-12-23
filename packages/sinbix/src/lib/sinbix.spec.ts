2ac645fb76a2b610d8b2ac85e817425403bf57fd
Alex Pol <sinbix001@gmail.com>
2020-12-23 17:50:24 +0300

    add sinbix

---
+++ packages/sinbix/src/lib/sinbix.spec.ts
@@ -0,0 +1,7 @@
+import { sinbix } from './sinbix';
+
+describe('sinbix', () => {
+  it('should work', () => {
+    expect(sinbix()).toEqual('sinbix');
+  });
+});
---
+++ packages/sinbix/src/lib/sinbix.ts
@@ -0,0 +1,3 @@
+export function sinbix(): string {
+  return 'sinbix';
+}
