5a62cd28d20fd7c176d8400a65e8b48b0581a06f
Alex Pol <sinbix001@gmail.com>
2021-02-06 13:25:43 +0300

    fix: common

--- patterns/plugin/schematic/schematic.ts
+++ patterns/plugin/schematic/schematic.ts
@@ -1,13 +1,17 @@
 import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
+import { Tree } from '@angular-devkit/schematics/src/tree/interface';
 import { normalizeOptions, PatternSchematicOptions } from './utils';
 
 export default function (options: PatternSchematicOptions): Rule {
-  const normalizedOptions = normalizeOptions(options);
-  return chain([
-    externalSchematic('@sinbix/common', 'project', {
-      name: normalizedOptions.name,
-      directory: normalizedOptions.directory,
-      tags: [...normalizedOptions.projectTags].join(','),
-    }),
-  ]);
+  return (host: Tree) => {
+    const normalizedOptions = normalizeOptions(host, options);
+
+    return chain([
+      externalSchematic('@sinbix/common', 'project', {
+        name: normalizedOptions.name,
+        directory: normalizedOptions.directory,
+        tags: [...normalizedOptions.projectTags].join(','),
+      }),
+    ]);
+  };
 }
--- patterns/plugin/schematic/utils/normalize-options.ts
+++ patterns/plugin/schematic/utils/normalize-options.ts
@@ -1,7 +1,9 @@
 import { NormalizedOptions, PatternSchematicOptions } from './models';
 import { normalizeProject } from '@sinbix/common';
+import { Tree } from '@angular-devkit/schematics/src/tree/interface';
 
 export function normalizeOptions(
+  host: Tree,
   options: PatternSchematicOptions
 ): NormalizedOptions {
   const normalizedProject = normalizeProject(options);
