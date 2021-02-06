192fc1187e04992bf59dee15d6dd434e8ae8537f
Alex Pol <sinbix001@gmail.com>
2021-02-06 10:29:10 +0300

    refactor: change versions

--- packages/packages/core/src/utils/versions.ts
+++ packages/packages/core/src/utils/versions.ts
@@ -1,4 +1,4 @@
-export const sinbixVersion = '0.0.1-alpha.224';
+export const sinbixVersion = '0.0.1-alpha.225';
 
 export const angularCliVersion = '~10.1.3';
 export const typescriptVersion = '~4.0.3';
