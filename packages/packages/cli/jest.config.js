5f8df02f2aed33c20787373a5859722fb9c074f0
Alex Pol <sinbix001@gmail.com>
2021-02-06 09:52:12 +0300

    refactor: move cli to packages/cli

--- packages/packages/cli/jest.config.js
+++ packages/packages/cli/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'cli',
-  preset: '../../jest.preset.js',
+  displayName: 'packages-cli',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/cli',
+  coverageDirectory: '../../../coverage/packages/packages/cli',
 };
