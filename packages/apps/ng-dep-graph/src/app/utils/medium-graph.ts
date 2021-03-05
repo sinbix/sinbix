dd9934f93ee77426d4c808096de3009d106e4299
Alex Pol <sinbix001@gmail.com>
2021-03-05 19:16:06 +0300

    feat: ng-dep-graph common code

---
+++ packages/apps/ng-dep-graph/src/app/utils/medium-graph.ts
@@ -0,0 +1,697 @@
+export const mediumGraph = {
+  version: '2.0',
+  rootFiles: [
+    {
+      file: 'angular.json',
+      hash: '3ea87635d14f46a32b56bd010061c4cdb225fc6f',
+      ext: '.json',
+    },
+    {
+      file: 'sinbix.json',
+      hash: '99b41a6538eb767e13b15058008bc8b73e7eaf29',
+      ext: '.json',
+    },
+    {
+      file: 'package.json',
+      hash: '7a4c917e70a48dcb884d07ac24c6e8133041f65d',
+      ext: '.json',
+    },
+    {
+      file: 'tsconfig.base.json',
+      hash: '3c41a2d2051ca18d36ad128197fabe0146d913de',
+      ext: '.json',
+    },
+  ],
+  nodes: {
+    'apps-nest-app': {
+      name: 'apps-nest-app',
+      type: 'app',
+      data: {
+        projectType: 'application',
+        root: 'apps/nest-app',
+        tags: ['platform:nest', 'type:app', 'scope:nest-app'],
+      },
+      files: []
+    },
+    'apps-nest-ms-nats': {
+      name: 'apps-nest-ms-nats',
+      type: 'app',
+      data: {
+        projectType: 'application',
+        root: 'apps/nest-ms-nats',
+        tags: ['platform:nest', 'type:app', 'scope:nest-ms-nats'],
+      },
+      files: []
+    },
+    'apps-nest-ms-redis': {
+      name: 'apps-nest-ms-redis',
+      type: 'app',
+      data: {
+        projectType: 'application',
+        root: 'apps/nest-ms-nats',
+        tags: ['platform:nest', 'type:app', 'scope:nest-ms-redis'],
+      },
+      files: []
+    },
+    'apps-ng-desktop-app': {
+      name: 'apps-ng-desktop-app',
+      type: 'app',
+      data: {
+        projectType: 'application',
+        root: 'apps/ng-desktop-app',
+        tags: ['platform:angular', 'type:app', 'scope:ng-desktop-app'],
+      },
+      files: []
+    },
+    'apps-ng-mobile-app': {
+      name: 'apps-ng-mobile-app',
+      type: 'app',
+      data: {
+        projectType: 'application',
+        root: 'apps/ng-mobile-app',
+        tags: ['platform:angular', 'type:app', 'scope:ng-mobile-app'],
+      },
+      files: []
+    },
+    'apps-ng-web-app': {
+      name: 'apps-ng-web-app',
+      type: 'app',
+      data: {
+        projectType: 'application',
+        root: 'apps/ng-web-app',
+        tags: ['platform:angular', 'type:app', 'scope:ng-web-app'],
+      },
+      files: []
+    },
+    'libs-nest-app-ui-auth': {
+      name: 'libs-nest-app-ui-auth',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-app/ui/auth',
+        tags: ['platform:nest', 'type:ui', 'scope:nest-app'],
+      },
+      files: []
+    },
+    'libs-nest-ms-app-ui-common': {
+      name: 'libs-nest-app-ui-common',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-app/ui/common',
+        tags: ['platform:nest', 'type:ui', 'scope:nest-app'],
+      },
+      files: []
+    },
+    'libs-nest-ms-nats-db-postgres': {
+      name: 'libs-nest-ms-nats-db-postgres',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-ms-nats/db/postgres',
+        tags: ['platform:nest', 'type:db', 'scope:nest-ms-nats'],
+      },
+      files: []
+    },
+    'libs-nest-ms-nats-services-auth': {
+      name: 'libs-nest-ms-nats-services-auth',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-ms-nats/services/auth',
+        tags: ['platform:nest', 'type:services', 'scope:nest-ms-nats'],
+      },
+      files: []
+    },
+    'libs-nest-ms-nats-services-common': {
+      name: 'libs-nest-ms-nats-services-common',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-ms-nats/services/common',
+        tags: ['platform:nest', 'type:services', 'scope:nest-ms-nats'],
+      },
+      files: []
+    },
+    'libs-nest-ms-nats-ui-auth': {
+      name: 'libs-nest-ms-nats-ui-auth',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-ms-nats/ui/auth',
+        tags: ['platform:nest', 'type:ui', 'scope:nest-ms-nats'],
+      },
+      files: []
+    },
+    'libs-nest-ms-nats-ui-common': {
+      name: 'libs-nest-ms-nats-ui-common',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-ms-nats/ui/common',
+        tags: ['platform:nest', 'type:ui', 'scope:nest-ms-nats'],
+      },
+      files: []
+    },
+    'libs-nest-ms-redis-db-mongo': {
+      name: 'libs-nest-ms-redis-db-mongo',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-ms-redis/db/mongo',
+        tags: ['platform:nest', 'type:db', 'scope:nest-ms-redis'],
+      },
+      files: []
+    },
+    'libs-nest-ms-redis-services-common': {
+      name: 'libs-nest-ms-redis-services-common',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-ms-redis/services/common',
+        tags: ['platform:nest', 'type:services', 'scope:nest-ms-redis'],
+      },
+      files: []
+    },
+    'libs-nest-ms-redis-ui-common': {
+      name: 'libs-nest-ms-redis-ui-common',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/nest-ms-redis/ui/common',
+        tags: ['platform:nest', 'type:ui', 'scope:nest-ms-redis'],
+      },
+      files: []
+    },
+    'libs-ng-desktop-app-assets': {
+      name: 'libs-ng-desktop-app-assets',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/ng-desktop-app/assets',
+        tags: ['platform:web', 'type:assets', 'scope:ng-desktop-app'],
+      },
+      files: []
+    },
+    'libs-ng-desktop-app-styles': {
+      name: 'libs-ng-desktop-app-styles',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/ng-desktop-app/styles',
+        tags: ['platform:web', 'type:styles', 'scope:ng-desktop-app'],
+      },
+      files: []
+    },
+    'libs-ng-mobile-app-assets': {
+      name: 'libs-ng-mobile-app-assets',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/ng-mobile-app/assets',
+        tags: ['platform:web', 'type:assets', 'scope:ng-mobile-app'],
+      },
+      files: []
+    },
+    'libs-ng-mobile-app-features-main': {
+      name: 'libs-ng-mobile-app-features-main',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/ng-mobile-app/features/main',
+        tags: ['platform:angular', 'type:features', 'scope:ng-mobile-app'],
+      },
+      files: []
+    },
+    'libs-ng-mobile-app-styles': {
+      name: 'libs-ng-mobile-app-styles',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/ng-mobile-app/styles',
+        tags: ['platform:web', 'type:styles', 'scope:ng-mobile-app'],
+      },
+      files: []
+    },
+    'libs-ng-web-app-assets': {
+      name: 'libs-ng-web-app-assets',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/ng-web-app/assets',
+        tags: ['platform:web', 'type:assets', 'scope:ng-web-app'],
+      },
+      files: []
+    },
+    'libs-ng-web-app-styles': {
+      name: 'libs-ng-web-app-styles',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/ng-web-app/styles',
+        tags: ['platform:web', 'type:styles', 'scope:ng-web-app'],
+      },
+      files: []
+    },
+    'libs-shared-common-domain': {
+      name: 'libs-shared-common-domain',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/shared/common/domain',
+        tags: ['platform:any', 'type:domain', 'scope:shared'],
+      },
+      files: []
+    },
+    'libs-shared-ng-assets-logo': {
+      name: 'libs-shared-ng-assets-logo',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/shared/ng/assets/logo',
+        tags: ['platform:web', 'type:assets', 'scope:shared'],
+      },
+      files: []
+    },
+    'libs-shared-ng-data-common': {
+      name: 'libs-shared-ng-data-common',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/shared/ng/data/common',
+        tags: ['platform:angular', 'type:data', 'scope:shared'],
+      },
+      files: []
+    },
+    'libs-shared-ng-styles-material': {
+      name: 'libs-shared-ng-styles-material',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/shared/ng/styles/material',
+        tags: ['platform:web', 'type:styles', 'scope:shared'],
+      },
+      files: []
+    },
+    'libs-shared-ng-features-auth': {
+      name: 'libs-shared-ng-features-auth',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/shared/ng/features/auth',
+        tags: ['platform:angular', 'type:features', 'scope:shared'],
+      },
+      files: []
+    },
+    'libs-shared-ng-features-main': {
+      name: 'libs-shared-ng-features-main',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/shared/ng/features/main',
+        tags: ['platform:angular', 'type:features', 'scope:shared'],
+      },
+      files: []
+    },
+    'libs-shared-ng-services-auth': {
+      name: 'libs-shared-ng-services-auth',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/shared/ng/services/auth',
+        tags: ['platform:angular', 'type:services', 'scope:shared'],
+      },
+      files: []
+    },
+    'libs-shared-ng-services-common': {
+      name: 'libs-shared-ng-services-common',
+      type: 'lib',
+      data: {
+        projectType: 'library',
+        root: 'libs/shared/ng/services/common',
+        tags: ['platform:angular', 'type:services', 'scope:shared'],
+      },
+      files: []
+    },
+  },
+  dependencies: {
+    'apps-nest-app': [
+      {
+        type: 'static',
+        source: 'apps-nest-app',
+        target: 'libs-nest-app-ui-common',
+      },
+      {
+        type: 'static',
+        source: 'apps-nest-app',
+        target: 'libs-nest-app-ui-auth',
+      },
+      {
+        type: 'static',
+        source: 'apps-nest-app',
+        target: 'libs-nest-app-shell',
+      }
+    ],
+    'apps-nest-ms-nats': [
+      {
+        type: 'static',
+        source: 'apps-nest-ms-nats',
+        target: 'libs-nest-ms-nats-db-postgres',
+      },
+      {
+        type: 'static',
+        source: 'apps-nest-ms-nats',
+        target: 'libs-nest-ms-nats-ui-common',
+      },
+      {
+        type: 'static',
+        source: 'apps-nest-ms-nats',
+        target: 'libs-nest-ms-nats-ui-auth',
+      },
+      {
+        type: 'static',
+        source: 'apps-nest-ms-nats',
+        target: 'libs-nest-ms-nats-shell',
+      }
+    ],
+    'apps-nest-ms-redis': [
+      {
+        type: 'static',
+        source: 'apps-nest-ms-redis',
+        target: 'libs-nest-ms-redis-db-mongo',
+      },
+      {
+        type: 'static',
+        source: 'apps-nest-ms-redis',
+        target: 'libs-nest-ms-redis-ui-common',
+      },
+      {
+        type: 'static',
+        source: 'apps-nest-ms-redis',
+        target: 'libs-nest-ms-redis-shell',
+      }
+    ],
+    'apps-ng-desktop-app': [
+      {
+        type: 'dynamic',
+        source: 'apps-ng-desktop-app',
+        target: 'libs-shared-ng-features-main',
+      },
+      {
+        type: 'dynamic',
+        source: 'apps-ng-desktop-app',
+        target: 'libs-shared-ng-features-auth',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-desktop-app',
+        target: 'libs-shared-ng-services-common',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-desktop-app',
+        target: 'libs-shared-ng-data-common',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-desktop-app',
+        target: 'libs-ng-desktop-app-styles',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-desktop-app',
+        target: 'libs-ng-desktop-app-assets',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-desktop-app',
+        target: 'libs-shared-ng-assets-logo',
+      }
+    ],
+    'apps-ng-mobile-app': [
+      {
+        type: 'dynamic',
+        source: 'apps-ng-mobile-app',
+        target: 'libs-ng-mobile-app-features-main',
+      },
+      {
+        type: 'dynamic',
+        source: 'apps-ng-mobile-app',
+        target: 'libs-shared-ng-features-auth',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-mobile-app',
+        target: 'libs-shared-ng-services-common',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-mobile-app',
+        target: 'libs-shared-ng-data-common',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-mobile-app',
+        target: 'libs-ng-mobile-app-shell',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-mobile-app',
+        target: 'libs-ng-mobile-app-styles',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-mobile-app',
+        target: 'libs-ng-mobile-app-assets',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-mobile-app',
+        target: 'libs-shared-ng-assets-logo',
+      }
+    ],
+    'apps-ng-web-app': [
+      {
+        type: 'dynamic',
+        source: 'apps-ng-web-app',
+        target: 'libs-shared-ng-features-main',
+      },
+      {
+        type: 'dynamic',
+        source: 'apps-ng-web-app',
+        target: 'libs-shared-ng-features-auth',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-web-app',
+        target: 'libs-shared-ng-services-common',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-web-app',
+        target: 'libs-shared-ng-data-common',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-web-app',
+        target: 'libs-ng-web-app-shell',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-web-app',
+        target: 'libs-ng-web-app-styles',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-web-app',
+        target: 'libs-ng-web-app-assets',
+      },
+      {
+        type: 'static',
+        source: 'apps-ng-web-app',
+        target: 'libs-shared-ng-assets-logo',
+      }
+    ],
+    'libs-ng-desktop-app-styles': [
+      {
+        type: 'static',
+        source: 'libs-ng-desktop-app-styles',
+        target: 'libs-shared-ng-styles-material',
+      },
+    ],
+    'libs-ng-mobile-app-features-main': [
+      {
+        type: 'static',
+        source: 'libs-ng-mobile-app-features-main',
+        target: 'libs-shared-ng-services-common',
+      },
+      {
+        type: 'static',
+        source: 'libs-ng-mobile-app-features-main',
+        target: 'libs-shared-common-domain',
+      },
+    ],
+    'libs-ng-mobile-app-styles': [
+      {
+        type: 'static',
+        source: 'libs-ng-mobile-app-styles',
+        target: 'libs-shared-ng-styles-material',
+      },
+    ],
+    'libs-ng-web-app-styles': [
+      {
+        type: 'static',
+        source: 'libs-ng-web-app-styles',
+        target: 'libs-shared-ng-styles-material',
+      },
+    ],
+    'libs-nest-app-ui-common': [
+      {
+        type: 'static',
+        source: 'libs-nest-app-ui-common',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-nest-app-ui-auth': [
+      {
+        type: 'static',
+        source: 'libs-nest-app-ui-auth',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-nest-ms-nats-db-postgres': [
+      {
+        type: 'static',
+        source: 'libs-nest-ms-nats-db-postgres',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-nest-ms-nats-services-common': [
+      {
+        type: 'static',
+        source: 'libs-nest-ms-nats-services-common',
+        target: 'libs-nest-ms-nats-db-postgres',
+      },
+      {
+        type: 'static',
+        source: 'libs-nest-ms-nats-services-common',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-nest-ms-nats-services-auth': [
+      {
+        type: 'static',
+        source: 'libs-nest-ms-nats-services-auth',
+        target: 'libs-nest-ms-nats-db-postgres',
+      },
+      {
+        type: 'static',
+        source: 'libs-nest-ms-nats-services-auth',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-nest-ms-nats-ui-common': [
+      {
+        type: 'static',
+        source: 'libs-nest-ms-nats-ui-common',
+        target: 'libs-nest-ms-nats-services-common',
+      },
+      {
+        type: 'static',
+        source: 'libs-nest-ms-nats-ui-common',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-nest-ms-nats-ui-auth': [
+      {
+        type: 'static',
+        source: 'libs-nest-ms-nats-ui-auth',
+        target: 'libs-nest-ms-nats-services-auth',
+      },
+      {
+        type: 'static',
+        source: 'libs-nest-ms-nats-ui-auth',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-nest-ms-redis-db-mongo': [
+      {
+        type: 'static',
+        source: 'libs-nest-ms-redis-db-mongo',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-nest-ms-redis-services-common': [
+      {
+        type: 'static',
+        source: 'libs-nest-ms-redis-services-common',
+        target: 'libs-nest-ms-redis-db-mongo',
+      },
+      {
+        type: 'static',
+        source: 'libs-nest-ms-redis-services-common',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-nest-ms-redis-ui-common': [
+      {
+        type: 'static',
+        source: 'libs-nest-ms-redis-services-common',
+        target: 'libs-nest-ms-redis-services-common',
+      },
+      {
+        type: 'static',
+        source: 'libs-nest-ms-redis-services-common',
+        target: 'libs-shared-common-domain',
+      }
+    ],
+    'libs-shared-ng-features-auth': [
+      {
+        type: 'static',
+        source: 'libs-shared-ng-features-auth',
+        target: 'libs-shared-ng-services-auth',
+      },
+      {
+        type: 'static',
+        source: 'libs-shared-ng-features-auth',
+        target: 'libs-shared-common-domain',
+      },
+    ],
+    'libs-shared-ng-features-main': [
+      {
+        type: 'static',
+        source: 'libs-shared-ng-features-main',
+        target: 'libs-shared-ng-services-common',
+      },
+      {
+        type: 'static',
+        source: 'libs-shared-ng-features-main',
+        target: 'libs-shared-common-domain',
+      },
+    ],
+    'libs-shared-ng-services-auth': [
+      {
+        type: 'static',
+        source: 'libs-shared-ng-services-auth',
+        target: 'libs-shared-common-domain',
+      },
+      {
+        type: 'static',
+        source: 'libs-shared-ng-services-auth',
+        target: 'libs-shared-ng-data-common',
+      },
+    ],
+    'libs-shared-ng-services-common': [
+      {
+        type: 'static',
+        source: 'libs-shared-ng-services-common',
+        target: 'libs-shared-common-domain',
+      },
+      {
+        type: 'static',
+        source: 'libs-shared-ng-services-common',
+        target: 'libs-shared-ng-data-common',
+      },
+    ],
+  }
+};
---
+++ packages/apps/ng-dep-graph/src/app/utils/models.ts
@@ -0,0 +1,11 @@
+import { ProjectGraph, ProjectGraphNode } from '@sinbix/core';
+
+export interface GraphModel {
+  projects: ProjectGraphNode[];
+  graph: ProjectGraph;
+  filteredProjects: ProjectGraphNode[];
+  affected: string[];
+  exclude: string[];
+  focusedProject: string;
+  groupByFolder: boolean;
+}
