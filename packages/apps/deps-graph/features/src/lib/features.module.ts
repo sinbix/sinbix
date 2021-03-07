f26f60dec64f47dfbcbeea547590a8fee09f800c
Alex Pol <sinbix001@gmail.com>
2021-03-07 11:19:20 +0300

    refactor: deps-graph

--- packages/apps/deps-graph/features/src/lib/features.module.ts
+++ packages/apps/deps-graph/features/src/lib/features.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FeaturesComponent } from './features.component';
+import { ROUTES } from './utils';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(ROUTES)],
+  declarations: [FeaturesComponent],
+  exports: [FeaturesComponent],
 })
 export class DepsGraphFeaturesModule {}
---
+++ packages/apps/deps-graph/features/src/lib/utils/routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { FeaturesComponent } from '../features.component';
+
+export const ROUTES: Routes = [
+  {
+    path: '',
+    component: FeaturesComponent,
+  },
+];
