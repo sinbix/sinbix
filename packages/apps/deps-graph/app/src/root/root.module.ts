f82e302b2160ae03679b5505f73e93c62d0617c1
Alex Pol <sinbix001@gmail.com>
2021-03-09 11:12:08 +0300

    feat: add akita

--- packages/apps/deps-graph/app/src/root/root.module.ts
+++ packages/apps/deps-graph/app/src/root/root.module.ts
@@ -4,6 +4,9 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RootComponent } from './root.component';
 import { ROUTES } from './utils';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { environment } from '@sinbix/apps/deps-graph/utils';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 
 @NgModule({
   declarations: [RootComponent],
@@ -11,6 +14,8 @@ import { ROUTES } from './utils';
     BrowserModule,
     RouterModule.forRoot(ROUTES, { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    AkitaNgRouterStoreModule,
   ],
   providers: [],
   bootstrap: [RootComponent],
