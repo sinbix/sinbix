33040387fdda472a90a2d2af16bda13b52c07bde
Alex Pol <sinbix001@gmail.com>
2021-03-10 12:35:19 +0300

    feat: add graph module

---
+++ packages/apps/deps-graph/ui/src/lib/graph/graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'deps-graph-ui-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.scss']
+})
+export class GraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
