7c75e3cd69810c730b0669d849866520f57198a2
Alex Pol <sinbix001@gmail.com>
2021-03-21 16:06:44 +0300

    refactor: graphql module

--- packages/packages/nest/common/src/graphql/graphql.module.ts
+++ packages/packages/nest/common/src/graphql/graphql.module.ts
@@ -9,34 +9,8 @@ export class GraphqlModule {
         GraphQLModule.forRoot({
           include: modules,
           autoSchemaFile: true,
-          context: ({ req }) => ({ req }),
           formatError: (err) => {
-            let message = err.message;
-
-            const badMessage =
-              'Unexpected error value: { status: "error", message: "';
-
-            if (message.includes(badMessage)) {
-              message = message
-                .replace(badMessage, '')
-                .slice(0, -3)
-                .replace(/(\\")/g, '"')
-                .replace(/(\\\\")/g, '\\"');
-            }
-
-            const exception = _.get(err, 'extensions.exception');
-
-            return {
-              message,
-              path: _.get(err, 'path'),
-              extensions: {
-                code: _.get(err, 'extensions.code'),
-                exception: {
-                  status: _.get(exception, 'status'),
-                  response: _.get(exception, 'response'),
-                },
-              },
-            };
+            return _.get(err, 'extensions.exception.response');
           },
         }),
         ...modules,
