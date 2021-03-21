968a374caafa11e88609df08d989767549cc23c5
Alex Pol <sinbix001@gmail.com>
2021-03-21 17:23:14 +0300

    refactor: test module

--- packages/demo/apps/nest/server-blog-ms/ui/src/test/test.module.ts
+++ packages/demo/apps/nest/server-blog-ms/ui/src/test/test.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@sinbix-nest/common';
 import { ServicesGameModule } from '@sinbix/demo/apps/nest/server/services';
-import { UtilsClientsModule } from '@sinbix/demo/apps/nest/server/utils';
 import { TestController } from './test.controller';
 
 @Module({
-  imports: [ServicesGameModule, UtilsClientsModule],
+  imports: [ServicesGameModule],
   controllers: [TestController],
 })
 export class UiTestModule {}
