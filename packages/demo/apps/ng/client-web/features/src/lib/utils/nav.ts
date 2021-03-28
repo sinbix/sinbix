6092b2faf82163ada9d41fa0c27a2995bda4ac4a
Alex Pol <sinbix001@gmail.com>
2021-03-28 19:50:20 +0300

    feat: add demo menu

--- packages/demo/apps/ng/client-web/features/src/lib/utils/nav.ts
+++ packages/demo/apps/ng/client-web/features/src/lib/utils/nav.ts
@@ -1,9 +1,10 @@
 import { NavItem } from '@sinbix-common/utils';
 
-export const MAIN_MENU: NavItem[] = [
+const MAIN_MENU_WITHOUT_DEMO: NavItem[] = [
   {
     title: 'home',
     url: '/',
+    exactMatch: true,
   },
   {
     title: 'blog',
@@ -12,6 +13,26 @@ export const MAIN_MENU: NavItem[] = [
   {
     title: 'games',
     url: '/games',
+  },
+];
+
+function getDemoMenu(levels: number[]) {
+  const menu = [];
+  const n = levels.shift();
+  for (let i = 0; i < n; i++) {
+    const level = { title: 'demo', children: [] };
+    level.children.push(...getDemoMenu([...levels]));
+    menu.push(...[...MAIN_MENU_WITHOUT_DEMO, level]);
+  }
+
+  return menu;
+}
+
+export const MAIN_MENU: NavItem[] = [
+  ...MAIN_MENU_WITHOUT_DEMO,
+  {
+    title: 'demo',
+    children: getDemoMenu([1, 2, 3, 4, 5, 4, 3, 2, 1]),
   },
 ];
 
