e888cb1d491094de13a6025879e2374fc29a7ac4
Alex Pol <sinbix001@gmail.com>
2021-03-28 16:48:35 +0300

    feat: add post

--- packages/demo/apps/ng/client-web/features/src/lib/blog/blog.module.ts
+++ packages/demo/apps/ng/client-web/features/src/lib/blog/blog.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BlogComponent } from './blog.component';
 import { RouterModule } from '@angular/router';
 import { BLOG_ROUTES } from './blog.routes';
+import { ItemComponent } from './ui/post/item/item.component';
+import { ListComponent } from './ui/post/list/list.component';
 
 @NgModule({
-  declarations: [BlogComponent],
+  declarations: [BlogComponent, ItemComponent, ListComponent],
   imports: [RouterModule.forChild(BLOG_ROUTES), CommonModule],
 })
 export class BlogModule {}
