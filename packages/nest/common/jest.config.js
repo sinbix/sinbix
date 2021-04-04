7a86fe1225369f187cad3a4b09879b618bfc266c
Alex Pol <sinbix001@gmail.com>
2021-04-04 16:02:40 +0300

    refactor: move nest packages

--- packages/nest/common/jest.config.js
+++ packages/nest/common/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'packages-nest-common',
-  preset: '../../../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/packages/packages/nest/common',
+  coverageDirectory: '../../../coverage/packages/packages/nest/common',
 };
