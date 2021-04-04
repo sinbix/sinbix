6738a5fadc7c80322cb49d8a910f60195da922c8
Alex Pol <sinbix001@gmail.com>
2021-04-04 15:43:38 +0300

    refactor: move angular-material

--- tools/webpacks/smat.ts
+++ tools/webpacks/smat.ts
@@ -4,7 +4,7 @@ import * as webpack from 'webpack';
 export default (config: webpack.Configuration) => {
   config.resolve.alias['@sinbix-angular/material/theming'] = path.resolve(
     __dirname,
-    '../../packages/packages/angular/material/_theming'
+    '../../packages/angular/material/_theming'
   );
   return config;
 };
