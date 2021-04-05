db4bbb95c726e35d40688dc1ff8e0bfb09bd21b5
Alex Pol <sinbix001@gmail.com>
2021-04-05 19:09:16 +0300

    refactor: move ms

--- demo/nest/apps/blog-ms/jest.config.js
+++ demo/nest/apps/blog-ms/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'demo-apps-nest-server-blog-ms-app',
-  preset: '../../../../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory:
-    '../../../../../../coverage/packages/demo/apps/nest/server-blog-ms/app',
+    '../../../../coverage/packages/demo/apps/nest/server-blog-ms/app',
 };
