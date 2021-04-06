f4567bcc5264d56ca7311512a0cd11b364fcbda8
Alex Pol <sinbix001@gmail.com>
2021-04-06 17:04:11 +0300

    refactor: common

--- patterns/plugin/builder/utils/models.ts
+++
@@ -1,5 +0,0 @@
-import { JsonObject } from '@angular-devkit/core';
-
-export interface PatternBuilderOptions extends JsonObject {}
-
-export interface NormalizedOptions extends PatternBuilderOptions {}
--- patterns/plugin/builder/utils/normalize-options.ts
+++
@@ -1,9 +0,0 @@
-import { NormalizedOptions, PatternBuilderOptions } from './models';
-
-export function normalizeOptions(
-  options: PatternBuilderOptions
-): NormalizedOptions {
-  return {
-    ...options,
-  };
-}
