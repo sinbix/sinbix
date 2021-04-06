a142acebe32f4d0f9f21619623c372b5d71f9ec1
Alex Pol <sinbix001@gmail.com>
2021-04-06 11:26:28 +0300

    refactor: move demo mob

--- demo/ng/apps/mob/jest.config.js
+++ demo/ng/apps/mob/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'demo-apps-ng-client-mob-app',
-  preset: '../../../../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -14,7 +14,7 @@ module.exports = {
       },
     },
   },
-  coverageDirectory: '../../../../../../coverage/packages/demo/apps/ng/client-mob/app',
+  coverageDirectory: '../../../../coverage/packages/demo/apps/ng/client-mob/app',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
